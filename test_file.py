115805421af83ea249b627af7c08e4bfcfe20713
KIRAN <kiranr8k@gmail.com>
2020-07-19 17:02:29 +0530

    practical ml

--- test_file.py
+++ test_file.py
@@ -26,49 +26,49 @@ X = np.array(ct.fit_transform(X))
 # Avoiding dummy variable trap
 X = X[:, 1:]
 
-# # # Split dataset
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-#
-# regressor = LinearRegression()
-# regressor.fit(X_train, y_train)
-#
-# y_pred = regressor.predict(X_test)
-#
-# print(y_test - y_pred)
-#
-# # Backward elimnation
-#
-# # statsmodel library does not take into account b0 constant
-# # need to add column of 1 in matrix of features
-#
-# X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
-#
-# # added column of 1s to matrix X, can also use statsmodels.tools.tools.add_constant(array)
-# #
-# # STEP 1: select significance level
-# # SL = 0.05
-#
-# # STEP 2: fit the model with all possible predictors
-# # Create optimal matrix of features
-# X_opt = X[:, [0, 1, 2, 3, 4, 5]]  # contains all independent variables
-# X_opt = np.array(X_opt, dtype=float)
-#
-# # create new regressor from statsmodel
-# # exog .. intercept is not included by default, needs to be added by the user (code at line 45)
-# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()  # ordinary least square
-#
-# c = list(regressor_OLS.pvalues)
-# print(c, "<--- p values ")
-# print(c.index(max(c)))
-# # print(np.where(c == max(c)))
-#
-# regressor_OLS.summary()
-#
-# print(np.__version__)
-# print(sm.__version__)
-# print(pd.__version__)
-#
+# # Split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+print(y_test - y_pred)
+
+# Backward elimnation
+
+# statsmodel library does not take into account b0 constant
+# need to add column of 1 in matrix of features
+
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+
+# added column of 1s to matrix X, can also use statsmodels.tools.tools.add_constant(array)
+#
+# STEP 1: select significance level
+# SL = 0.05
+#
+# STEP 2: fit the model with all possible predictors
+# Create optimal matrix of features
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]  # contains all independent variables
+X_opt = np.array(X_opt, dtype=float)
+
+# create new regressor from statsmodel
+# exog .. intercept is not included by default, needs to be added by the user (code at line 45)
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()  # ordinary least square
+
+c = list(regressor_OLS.pvalues)
+print(c, "<--- p values ")
+print(c.index(max(c)))
+# print(np.where(c == max(c)))
+
+regressor_OLS.summary()
+
+print(np.__version__)
+print(sm.__version__)
+print(pd.__version__)
+
+#
 # \begin{align}
 # \dot{y} & = a_0x_0 +  a_1x_1 + a_2x_2 + a_3x_3  \\
 # \end{align}
@@ -101,3 +101,11 @@ print(X_Modeled)
 from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
+
+
+from sklearn.linear_model import LogisticRegression
+classifier =  LogisticRegression(random_state= 0)
+classifier.fit(X_train, y_train)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix()
