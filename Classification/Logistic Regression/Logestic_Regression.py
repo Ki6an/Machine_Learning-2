eb10bb7eb924d6bcd53e00357bd916edb19bcbfb
KIRAN <kiranr8k@gmail.com>
2020-07-30 21:54:36 +0530

    practical ml

--- Classification/Logistic Regression/Logestic_Regression.py
+++ Classification/Logistic Regression/Logestic_Regression.py
@@ -70,6 +70,8 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c=ListedColormap(('yellow', 'blue'))(i), label=j)
+
+
 plt.title('Logistic Regression (test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
