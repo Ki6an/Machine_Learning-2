1f6b7d2b21c58ff4c31f137265fa12edc263337a
KIRAN <kiranr8k@gmail.com>
2020-08-07 13:15:30 +0530

    cnn

--- Tensorflow/basics.py
+++ Tensorflow/basics.py
@@ -1,19 +1,19 @@
 import tensorflow as tf
 
-# TF 2.0 supports eager execution so don't explicitly create a session and run the code in it, instead you it the conventional way
-
+# TF 2.0 supports eager execution so don't explicitly create a session and run the code in it, instead you it the
+# conventional way
 
 # old boring/ boilerplate coding method
 # Launch the graph in a session.
-with tf.compat.v1.Session() as ses:
-    # Build a graph.
-    a = tf.constant(5.0)
-    b = tf.constant(6.0)
-    c = a * b
-
-    # Evaluate the tensor `c`.
-    print(ses.run(c))
-    print("Multiplication with constants: %i" % ses.run(a * b))
+# with tf.compat.v1.Session() as ses:
+#     # Build a graph.
+#     a = tf.constant(5.0)
+#     b = tf.constant(6.0)
+#     c = a * b
+#
+#     # Evaluate the tensor `c`.
+#     print(ses.run(c))
+#     print("Multiplication with constants: %i" % ses.run(a * b))
 
 
 # to get rid of the above method we use one simple line
